dd80c2d82a1701d2b4eb86e380db28d8c38f44ee
Mani Chandra Dulam <mani.dchandra@juspay.in>
2024-03-09 17:46:48 +0530

    feat: third

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+    net::TcpStream,
+};
+
+fn get_data_from_streams(stream: &mut TcpStream) -> String {
+    let mut res = [0; 20];
+    let _ = stream.read_exact(&mut res);
+    String::from_utf8(res.to_vec()).unwrap()
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,15 +18,18 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let data = get_data_from_streams(&mut stream);
+                let x = data.split(' ').nth(1);
+                match x {
+                    Some("/") => stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+                    _ => stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
+                };
             }
-
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
